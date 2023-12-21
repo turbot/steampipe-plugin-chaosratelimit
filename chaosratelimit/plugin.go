9f3b3a6477612a52272d1441cde3c89d33c65170
kai <kai@turbot.com>
2023-12-21 14:24:04 +0000

    debug trivy build failure

--- chaosratelimit/plugin.go
+++ chaosratelimit/plugin.go
@@ -13,7 +13,6 @@ const (
 )
 
 func Plugin(_ context.Context) *plugin.Plugin {
-	//time.Sleep(10 * time.Second)
 	p := &plugin.Plugin{
 		Name: pluginName,
 		TableMap: map[string]*plugin.Table{
@@ -26,25 +25,25 @@ func Plugin(_ context.Context) *plugin.Plugin {
 		RateLimiters: []*rate_limiter.Definition{
 
 			//{
-			//	Name:       "limiter1",
+			//	Instance:       "limiter1",
 			//	FillRate:   2,
 			//	BucketSize: 1,
 			//},
 			//{
-			//	Name:       "limiter_rate scoped limiter, 1/s",
+			//	Instance:       "limiter_rate scoped limiter, 1/s",
 			//	FillRate:   1,
 			//	BucketSize: 1,
 			//	Scopes:     []string{"limiter_rate"},
 			//	Where:      "limiter_rate = '1'",
 			//},
 			//{
-			//	Name:       "region scoped limiter (column scope)",
+			//	Instance:       "region scoped limiter (column scope)",
 			//	FillRate:   10,
 			//	BucketSize: 10,
 			//	Scopes:     []string{rateLimiterScopeRegion},
 			//},
 			//{
-			//	Name:       "connection, region, service scoped limiter",
+			//	Instance:       "connection, region, service scoped limiter",
 			//	FillRate:   10,
 			//	BucketSize: 10,
 			//	Scopes:     []string{rateLimiterScopeRegion, rateLimiterScopeService, rate_limiter.RateLimiterScopeConnection},
